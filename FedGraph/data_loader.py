3a2ca9aa84b6ee3628428ef1e7d58f5ca28be434
MD. ATIK <140510725+MDATIK-3@users.noreply.github.com>
2026-01-19 16:03:35 +0600

    Update data_loader.py

--- FedGraph/data_loader.py
+++ FedGraph/data_loader.py
@@ -1,8 +1,3 @@
-"""
-get_data() : 获取SubGraph设置下的图数据
-load_partition_data() : 获取切分后的图数据
-"""
-
 import torch
 import numpy as np
 import networkx as nx
@@ -13,22 +8,11 @@ from data_update import *
 import os
 import os.path as osp
 
-# Automatically set ROOT for Kaggle / Colab / local
-ROOT = '/kaggle/working/FC-FedGCN/data' if 'kaggle' in os.getcwd() else osp.dirname(__file__)
+ROOT = '/kaggle/working/Thesis/data' if 'kaggle' in os.getcwd() else osp.dirname(__file__)
 DATA_NAME = ['Cora', 'CiteSeer', 'PubMed']
 
 def get_data(name):
-    """
-    Subgraph setting 下的数据集, 返回 PyG 数据对象
-
-    Args:
-        name (str): 数据集名称
-
-    Returns:
-        data (torch_geometric.data.Data)
-    """
     global data
-
     if name in DATA_NAME:
         dataset = Planetoid(root=ROOT, name=name)
         data = dataset[0]
@@ -45,7 +29,6 @@ def get_data(name):
     elif name == 'FaceBook':
         data = facebook_to_Tensor()
 
-    # 90% train / 10% test split
     train_num = int(len(data.x) * 0.9)
     all_train_mask = [i <= train_num for i in range(len(data.x))]
     all_test_mask  = [not x for x in all_train_mask]
@@ -55,30 +38,14 @@ def get_data(name):
     return data
 
 def load_partition_data(num_clients: int, data: Data, isImpaired=False):
-    """
-    将整图切分为多个子图 (客户端划分)
-
-    Args:
-        num_clients (int): 客户端数量
-        data (Data): PyG 图数据
-        isImpaired (bool): 是否考虑损失的link (可扩展)
-
-    Returns:
-        nodes_list (list[list[int]]): 每个客户端的节点列表
-    """
-    # 转换 PyG Data 为 NetworkX 图
     G = nx.Graph()
     G.add_nodes_from(range(data.x.shape[0]))
-    edges = np.array(data.edge_index.T, dtype=int)
+    edges = np.array(data.edge_index.cpu().T, dtype=int)
     G.add_edges_from(edges)
 
-    # 转换为 adjacency list 给 pymetis
     adjacency = [list(G.neighbors(n)) for n in range(len(G))]
-
-    # 使用 pymetis 划分
     n_cuts, parts = pymetis.part_graph(num_clients, adjacency=adjacency)
 
-    # 构建每个客户端节点列表
     nodes_list = [[] for _ in range(num_clients)]
     for node_id, part_id in enumerate(parts):
         nodes_list[part_id].append(node_id)
